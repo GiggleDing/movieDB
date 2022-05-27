b11dca8f85698ab179d83f5b55dce9c794ea8df6
jyx <2020302111038@whu.edu.cn>
2022-05-27 20:15:07 +0800

    1

--- Models/User.cs
+++ Models/User.cs
@@ -8,6 +8,7 @@ namespace MvcMovie.Models
         [Required, StringLength(maximumLength:50,MinimumLength = 1)]
         public string UserName { get; set; }
 
+
         [Required, StringLength(maximumLength:50,MinimumLength = 1)]
         public string UserPwd { get; set; }
 
